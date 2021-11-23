aaa5ab5c6a2612ba378de5cc31538da89089faaa
B3YD4 <41930266+B3YD4@users.noreply.github.com>
2021-11-23 13:39:03 +0300

    Update music.js

--- public/javascripts/music.js
+++ public/javascripts/music.js
@@ -19,7 +19,7 @@ prevBtn.style.display = "none";
 nextBtn.style.display = "none";
 
 // Song titles
-const songs = ['Duman - Dibine Kadar', 'Duman - Herşeyi Yak','Yaşlı Amca - İstanbul Beyefendisi' ,'Katy Perry - ET'];
+const songs = ['Duman - Dibine Kadar', 'Duman - Herşeyi Yak','Yaşlı Amca - İstanbul Beyefendisi' ,'Katy Perry - ET', 'Duman - Eski Köprünün Altında'];
 
 // Keep track of song
 let songIndex = 0;
